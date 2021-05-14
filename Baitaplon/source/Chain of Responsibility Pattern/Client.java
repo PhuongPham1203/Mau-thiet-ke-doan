d9ab71cdddcdaac05b39747425c040df921bf3c7
PhuongPham1203 <Phamphuong120398@gmail.com>
2021-05-14 12:57:03 +0700

    done Chain of Responsibility Pattern

---
+++ Baitaplon/source/Chain of Responsibility Pattern/Client.java
@@ -0,0 +1,22 @@
+
+public class Client {
+ 
+    public static void main(String[] args) {
+        // * Tạo chain of responsibility
+        Logger logger = AppLogger.getLogger();
+ 
+        // * thử log ở mức độ Debug
+        System.out.println("Log level Console");
+        //logger.log(LogLevel.INFO, "Info message");
+        logger.log(LogLevel.DEBUG, "Debug message");
+        //logger.log(LogLevel.WARNING, "Debug message");
+ 
+        // * thử log ở mức độ Error
+        System.out.println("----\nLog level Error - loi");
+        logger.log(LogLevel.ERROR, "Error message");
+ 
+        // * thử log ở mức độ Fatal
+        System.out.println("----\nLog level Fatal - cuc nguy hiem");
+        logger.log(LogLevel.FATAL, "Factal message");
+    }
+}
--- readme.txt
+++ readme.txt
@@ -2,3 +2,15 @@
 Link Github : https://github.com/PhuongPham1203/Mau-thiet-ke-doan.git
 
 Bài tập lớn được lưu tại Branch : bai-tap-lon
+
+Cách chạy code phần Chain of Responsibility Pattern :
+    1 : vào thư mục .\source\Chain of Responsibility Pattern
+    2 : javac *.java
+    3 : java Client
+
+
+Cách chạy code phần Interpreter Pattern :
+    1 : vào thư mục .\source\Interpreter Pattern
+    2 : javac *.java
+    3 : java Client
+    
